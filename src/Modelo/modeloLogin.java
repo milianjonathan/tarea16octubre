ce42ef906aae437cbbf603e14ac76fe50b581a20
alexisvides <68454869+alexisvides@users.noreply.github.com>
2020-10-13 18:43:50 -0600

    Update

--- src/Modelo/modeloLogin.java
+++ src/Modelo/modeloLogin.java
@@ -10,5 +10,31 @@ package Modelo;
  * @author ALEXIS VIDES
  */
 public class modeloLogin {
-    
+    private String Nombre;
+    private String Pass;
+    private String Rango;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getPass() {
+        return Pass;
+    }
+
+    public void setPass(String Pass) {
+        this.Pass = Pass;
+    }
+
+    public String getRango() {
+        return Rango;
+    }
+
+    public void setRango(String Rango) {
+        this.Rango = Rango;
+    }
 }
