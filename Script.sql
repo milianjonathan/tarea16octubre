29bcabcf1a177b876d2d32962bb5bfcb993cef6d
pabloT023 <66076694+pabloT023@users.noreply.github.com>
2020-10-13 21:38:19 -0600

    conector y script

---
+++ Script.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE TAREA_MVC;
+
+USE TAREA_MVC;
+
+CREATE TABLE libro (
+id_libro INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+genero_libro VARCHAR(100) NOT NULL,
+fecha_libro INT NOT NULL,
+nombre_libro VARCHAR(100) NOT NULL,
+autor_libro VARCHAR(100) NOT NULL,
+paginas_libro INT NOT NULL,
+contenido_libro VARCHAR(MAX) NOT NULL);
+
+CREATE PROCEDURE obtener_libros
+AS
+SELECT * FROM libro
+GO;
+
+CREATE TABLE TB_LOGIN(
+id_usuario INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+usuario VARCHAR(100) NOT NULL,
+pass VARCHAR(100) NOT NULL,
+rol INT NOT NULL);
+
+INSERT INTO TB_LOGIN VALUES('admin','1234',1),('lector','abcd',2);
+
+CREATE PROCEDURE obtener_usuarios
+AS
+SELECT * FROM TB_LOGIN
+GO;
+
+
+
+
+
+
+
+
